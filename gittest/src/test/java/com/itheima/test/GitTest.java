ce6576f09772d4d24d7e2482bfe4d7aea3506a0b
Ding0118 <2993246811@qq.com>
2020-08-18 19:38:23 +0800

    提交工程

---
+++ gittest/src/test/java/com/itheima/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+
+/**
+ * @description:
+ * @author: ding
+ * @date: 2020/8/18 19:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
